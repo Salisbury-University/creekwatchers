69d84a1d2209173af415544e6bd713dfe0119098
dylan larrabee <dylanlarrabee18@gmail.com>
2023-02-27 00:44:15 -0500

    alter login

---
+++ app/src/main/java/com/dylanlarrabee/watersurveyapp/LoginPage.java
@@ -0,0 +1,65 @@
+package com.dylanlarrabee.watersurveyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginPage extends AppCompatActivity {
+
+    //vars
+    int countClick = 0;
+    int clicked = 0;
+    String userName,siteName;
+    Button selName;
+    Intent toHome = new Intent(this,HomePage.class);
+    Intent toSelName = new Intent(this,SelectName.class);
+    Intent toSelSite = new Intent(this,SelectSite.class);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+
+        //Find views
+        EditText enterName = (EditText) findViewById(R.id.entername);
+        Button donebtn = (Button) findViewById(R.id.btndone);
+        TextView user = (TextView) findViewById(R.id.userName);
+        Animation fadeIn = AnimationUtils.loadAnimation(this,R.anim.fade_in);
+        Animation fadeOut = AnimationUtils.loadAnimation(this,R.anim.fade_out);
+        selName = (Button) findViewById(R.id.selNameLog);
+        //listeners
+        selName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nameView();
+            }
+        });
+        donebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+           if(enterName.getText().toString().length() < 20 && enterName.getText().toString().length() > 1)
+           {
+            toSelSite.putExtra("name",enterName.getText().toString());
+            startActivity(toSelSite);
+
+
+           }
+
+
+
+
+            }
+        });
+    }
+    void nameView()
+    {
+        startActivity(toSelName);
+    }
+
+}
---
+++ app/src/main/java/com/dylanlarrabee/watersurveyapp/SelectSite.java
@@ -0,0 +1,40 @@
+package com.dylanlarrabee.watersurveyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SelectSite extends AppCompatActivity {
+    final int numSites = 1;
+Button site1;
+Bundle getExtras;
+String userName;
+Intent toHome;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_site);
+        //intents
+        toHome = new Intent(this,HomePage.class);
+        //find views
+        site1 = findViewById(R.id.siteBtn);
+        getExtras = getIntent().getExtras();
+        userName = getExtras.getString("name");
+        //listeners
+        site1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            toHome(site1.getText().toString());
+            }
+        });
+    }
+    void toHome(String siteName)
+    {
+        toHome.putExtra("site",siteName);
+        toHome.putExtra("name",userName);
+    startActivity(toHome);
+    }
+}
