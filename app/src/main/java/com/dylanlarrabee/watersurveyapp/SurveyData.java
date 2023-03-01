7cb88b7467db35d61a0de882fd36f4697dbfc54e
dlarrabee18 <93663658+dlarrabee18@users.noreply.github.com>
2023-03-01 12:06:47 -0500

    data class added

---
+++ app/src/main/java/com/dylanlarrabee/watersurveyapp/SurveyData.java
@@ -0,0 +1,7 @@
+package com.dylanlarrabee.watersurveyapp;
+
+public class SurveyData {
+        int tideEst,weathEst,windSpeed,warterSurf,windDir,rainfall,waterDepth,sampleDist;
+        int[] airTemp,waterTemp,secchiDepth;
+        boolean bottomedOut;
+}
