b426fe798491c0b07732f91e3e14be2be3c572be
dylan larrabee <dylanlarrabee18@gmail.com>
2023-03-04 20:13:12 -0500

    nothing

--- app/src/main/java/com/dylanlarrabee/watersurveyapp/FormArchive.java
+++ app/src/main/java/com/dylanlarrabee/watersurveyapp/FormArchive.java
@@ -14,7 +14,6 @@ import android.widget.TextView;
 import com.google.gson.Gson;
 
 public class FormArchive extends AppCompatActivity {
-
     private Button newFormBtn;
     private LinearLayout formLay;
     SharedPreferences mPrefs;
