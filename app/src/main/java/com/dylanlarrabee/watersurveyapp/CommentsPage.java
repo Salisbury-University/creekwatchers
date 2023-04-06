e3011d8d9fbca0841d5c3fcec542471befc9cc09
sacosta1369 <103210057+sacosta1369@users.noreply.github.com>
2023-04-06 11:50:10 -0400

    Comments page working

--- app/src/main/java/com/dylanlarrabee/watersurveyapp/CommentsPage.java
+++ app/src/main/java/com/dylanlarrabee/watersurveyapp/CommentsPage.java
@@ -2,6 +2,8 @@ package com.dylanlarrabee.watersurveyapp;
 import static com.dylanlarrabee.watersurveyapp.R.*;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,6 +23,15 @@ public class CommentsPage extends AppCompatActivity{
         BasicCommands.setActivity(this, comm_h, HomePage.class);
         BasicCommands.setActivity(this, comm_b, SecchiDepth.class);
         BasicCommands.setActivity(this, comm_n, ReviewPage.class);
-    }
 
+        EditText inText = (EditText) findViewById(id.commentText);
+        SurveyData.comments = inText.getEditableText().toString();
+
+        CheckBox bottomOut = findViewById(id.bottomout);
+
+        if(SurveyData.secchiDepth[0] == SurveyData.waterDepth[0] || SurveyData.secchiDepth[1] == SurveyData.waterDepth[0]){
+            bottomOut.setChecked(true);
+            SurveyData.bottomedOut = true;
+        }
+    }
 }
