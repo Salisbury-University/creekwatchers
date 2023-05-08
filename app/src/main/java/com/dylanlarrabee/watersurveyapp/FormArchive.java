b0aa08435577fabdf14ada87e66c54b5807e3530
dlarrabee18 <93663658+dlarrabee18@users.noreply.github.com>
2023-05-07 21:28:10 -0400

    ui

--- app/src/main/java/com/dylanlarrabee/watersurveyapp/FormArchive.java
+++ app/src/main/java/com/dylanlarrabee/watersurveyapp/FormArchive.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -86,7 +87,6 @@ public class FormArchive extends SaveFormAct {
         View formView = getLayoutInflater().inflate(R.layout.form_card,null);
         TextView text = formView.findViewById(R.id.formName);
         TextView btn = formView.findViewById(R.id.cardDelBtn);
-        text.setText(formID);
         text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
